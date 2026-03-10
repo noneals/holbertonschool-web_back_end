52c8d12cef4c721fcca6266ee0ccf0d34dc0fd6b
noneals <124405266+noneals@users.noreply.github.com>
2026-03-10 21:35:16 +0300

    Refactor getBudgetObject to use object shorthand
    Refactor budget object creation for improved readability.

--- ES6_basic/7-getBudgetObject.js
+++ ES6_basic/7-getBudgetObject.js
@@ -1,4 +1,10 @@
 export default function getBudgetObject(income, gdp, capita) {
-  const budget = { income, gdp, capita };
+  // Utilizamos la sintaxis de atajo de valores de propiedad de objetos aquí.
+  const budget = {
+    income,
+    gdp,
+    capita,
+  };
+
   return budget;
 }
